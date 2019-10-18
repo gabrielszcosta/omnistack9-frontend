50919955cd7aad94e77043dd1e6cbd8cd4d84a8b
Gabriel Costa <gabrielszcosta@gmail.com>
2019-10-18 17:12:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [email, setEmail] = useState('');
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		const response = await api.post('/sessions', { email });
+	}
+
+	return (
+		<div className="container">
+			<img src={logo} alt="logo" />
+			<div className="content">
+				<p>
+					Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua
+					empresa
+				</p>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="email">E-MAIL *</label>
+					<input
+						type="email"
+						id="email"
+						placeholder="Seu melhor e-mail"
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+					/>
+					<button className="btn" type="submit">
+						Entrar
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
